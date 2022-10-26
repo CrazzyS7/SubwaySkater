3270a43d417ab8515855fcdb52fd2e4590ff78a4
CrazzyS7 <magagula.muzisandile@gmail.com>
2022-10-26 21:53:53 +0200

    PC controller activated from TowerController by removing the comments

--- Scripts/TowerController.cs
+++ Scripts/TowerController.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 
 public class TowerController : MonoBehaviour
@@ -6,11 +7,19 @@ public class TowerController : MonoBehaviour
 
     void Update()
     {
+        // for PC
         if(Input.GetMouseButton(0) && !GameManager.IsPaused)
         {
             float mouseX = Input.GetAxisRaw("Mouse X");
             transform.Rotate(0, -mouseX * mSpeed * Time.deltaTime, 0);
         }
+        /*
+        // for Mobile
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            float xDelta = Input.GetTouch(0).deltaPosition.x;
+            transform.Rotate(0, -xDelta * mSpeed * Time.deltaTime, 0);
+        }*/
         return;
     }
 }
